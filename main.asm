bbbca95a9fe9ee4261f36078902c4b1f97af95f8
Weverson <unifwpereira@fei.edu.br>
2020-10-20 19:23:45 -0300

    LCD display added

--- main.asm
+++ main.asm
@@ -1,16 +1,16 @@
-;=====================================================
+;==========================================================================================================
 ;                      HEADER;
-;=====================================================
+;==========================================================================================================
 ; This program seeks to automate a 
 ; movie theater using the 8051 
 ; microcontroller in the EdSim51 
 ; simulator
-;=====================================================
+;==========================================================================================================
 
 
-;=====================================================
+;==========================================================================================================
 ;                  MAIN SECTION
-;=====================================================
+;==========================================================================================================
 
 ; Jumps for the main function
 org 0000h
@@ -20,12 +20,14 @@ org 0000h
 ; Main function
 org 0080h
 Main:
-	MOV R1, #0
-	CALL showMovies
+	ACALL showMovies
+	ACALL lcd_init
+	ACALL askForTheMovie
 	SJMP $
-;=====================================================
+
+;==========================================================================================================
 ;              SERIAL CHANNEL SECTION
-;=====================================================
+;==========================================================================================================
 
 ; INTERRUPTION FOR RECEPTIONS
 org 0023H
@@ -123,4 +125,249 @@ moviesList:
 	db "1 » Dune - Starts in 2m" 
 	db '\n'
 	db "2 » 007-Again - Starts in 1m"
+	db 0
+
+;==========================================================================================================
+;                  LCD DISPLAY SECTION
+;==========================================================================================================
+org 01A0h
+
+; Ask for the movie in the lcd display
+askForTheMovie:
+	MOV A, #02h ; |  Start position in the first column
+	ACALL posicionaCursor
+	MOV DPTR,#afts1	        ; |  DPTR = begin of the phrase in the first column
+	ACALL escreveString
+	MOV A, #45h ; |  Start position in the second column
+	ACALL posicionaCursor
+	MOV DPTR,#aftm2 	    ; |  DPTR = begin of the phrase in the second column
+    ACALL escreveString
+	RET
+	aftm1:
+		db "Selecione um"
+		db 0
+	aftm2: 
+		db "filme"
+		db 0
+
+; Ask for the seat in the lcd display
+askForTheSeat:
+	MOV A, #01h ; |  Start position in the first column
+	ACALL posicionaCursor
+	MOV DPTR,#afts1	        ; |  DPTR = begin of the phrase in the first column
+	ACALL escreveString
+	MOV A, #44h  ; |  Start position in the second column
+	ACALL posicionaCursor
+	MOV DPTR,#afts2 	    ; |  DPTR = begin of the phrase in the second column
+    ACALL escreveString
+	RET
+	afts1:
+		db "Selecione uma"
+		db 0
+	afts2: 
+		db "poltrona"
+		db 0
+
+escreveString:
+		MOV R2, #0
+rot:
+		MOV A, R2
+ 		MOVC A,@A+DPTR 		;lê a tabela da memória de programa
+ 		ACALL sendCharacter	; send data in A to LCD module
+		INC R2
+		JNZ rot		; if A is 0, then end of data has been reached - jump out of loop
+ 		RET
+
+; --- Mapeamento de Hardware (8051) ---
+    RS      equ     P1.3    ;Reg Select ligado em P1.3
+    EN      equ     P1.2    ;Enable ligado em P1.2
+
+; initialise the display
+; see instruction set for details
+lcd_init:
+
+	CLR RS		; clear RS - indicates that instructions are being sent to the module
+
+; function set	
+	CLR P1.7		; |
+	CLR P1.6		; |
+	SETB P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear	
+					; function set sent for first time - tells module to go into 4-bit mode
+; Why is function set high nibble sent twice? See 4-bit operation on pages 39 and 42 of HD44780.pdf.
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+					; same function set high nibble sent a second time
+
+	SETB P1.7		; low nibble set (only P1.7 needed to be changed)
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+				; function set low nibble sent
+	CALL delay		; wait for BF to clear
+
+
+; entry mode set
+; set to increment with no shift
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	SETB P1.6		; |
+	SETB P1.5		; |low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+
+
+; display on/off control
+; the display is turned on, the cursor is turned on and blinking is turned on
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	SETB P1.7		; |
+	SETB P1.6		; |
+	SETB P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+	RET
+
+
+sendCharacter:
+	SETB RS  		; setb RS - indicates that data is being sent to module
+	MOV C, ACC.7		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.6		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.5		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.4		; |
+	MOV P1.4, C			; | high nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	MOV C, ACC.3		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.2		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.1		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.0		; |
+	MOV P1.4, C			; | low nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	CALL delay			; wait for BF to clear
+	CALL delay			; wait for BF to clear
+	RET
+
+;Posiciona o cursor na linha e coluna desejada.
+;Escreva no Acumulador o valor de endereço da linha e coluna.
+;|--------------------------------------------------------------------------------------|
+;|linha 1 | 00 | 01 | 02 | 03 | 04 |05 | 06 | 07 | 08 | 09 |0A | 0B | 0C | 0D | 0E | 0F |
+;|linha 2 | 40 | 41 | 42 | 43 | 44 |45 | 46 | 47 | 48 | 49 |4A | 4B | 4C | 4D | 4E | 4F |
+;|--------------------------------------------------------------------------------------|
+posicionaCursor:
+	CLR RS	
+	SETB P1.7		    ; |
+	MOV C, ACC.6		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.5		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.4		; |
+	MOV P1.4, C			; | high nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	MOV C, ACC.3		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.2		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.1		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.0		; |
+	MOV P1.4, C			; | low nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	CALL delay			; wait for BF to clear
+	CALL delay			; wait for BF to clear
+	RET
+
+
+;Retorna o cursor para primeira posição sem limpar o display
+retornaCursor:
+	CLR RS	
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CLR P1.7		; |
+	CLR P1.6		; |
+	SETB P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+	RET
+
+
+;Limpa o display
+clearDisplay:
+	CLR RS	
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+	RET
+
+
+delay:
+	MOV R0, #50
+	DJNZ R0, $
+	RET
 
